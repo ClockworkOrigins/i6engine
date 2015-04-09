5ec46aad569cf9db116940b7d8d7e4de7e31c789
Daniel Bonrath <Big-DB@web.de>
2015-04-09 16:20:03 +0200

    ISIXE-909 removed some threads from sharedPtr stress test to get it working with valgrind

--- test/i6engine-utils/test_sharedPtr.cpp
+++ test/i6engine-utils/test_sharedPtr.cpp
@@ -331,10 +331,10 @@ TEST_F(sharedPtr, multiThread) {
 
 	{
 		Ptr1 p = Ptr1(new TestObject1());
-		for(int i = 0; i < 1000; i++) {
+		for(int i = 0; i < 100; i++) {
 			v.push_back(new boost::thread(boost::bind(spawn, p)));
 		}
-		for (size_t i = 0; i < 1000; i++) {
+		for (size_t i = 0; i < 100; i++) {
 			Ptr1::clear();
 			v[i]->join();
 			delete v[i];
