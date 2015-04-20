9549326103404bb840923d5e0116acca08947d96
Daniel Bonrath <Big-DB@web.de>
2015-04-20 11:01:11 +0200

    ISIXE-1390 RPG components now are also registered in SampleLeveleditor

--- samples/leveleditor/src/LeveleditorApplication.cpp
+++ samples/leveleditor/src/LeveleditorApplication.cpp
@@ -20,6 +20,21 @@
 #include "i6engine/api/configs/InputConfig.h"
 #include "i6engine/api/facades/InputFacade.h"
 
+#ifdef ISIXE_WITH_RPG
+	#include "i6engine/api/facades/ObjectFacade.h"
+
+	#include "i6engine/rpg/components/AttributeComponent.h"
+	#include "i6engine/rpg/components/HealthbarComponent.h"
+	#include "i6engine/rpg/components/ListInventoryComponent.h"
+	#include "i6engine/rpg/components/NameComponent.h"
+	#include "i6engine/rpg/components/QuickslotComponent.h"
+	#include "i6engine/rpg/components/SlotComponent.h"
+	#include "i6engine/rpg/components/SlotInventoryComponent.h"
+	#include "i6engine/rpg/components/ThirdPersonControlComponent.h"
+	#include "i6engine/rpg/components/UsableItemComponent.h"
+	#include "i6engine/rpg/components/WeightInventoryComponent.h"
+#endif
+
 #include "boost/bind.hpp"
 
 namespace sample {
@@ -32,6 +47,21 @@ namespace sample {
 
 	void LeveleditorApplication::AfterInitialize() {
 		Editor::AfterInitialize();
+
+#ifdef ISIXE_WITH_RPG
+		// register rpg components we want to use
+		// do this befor loading the level
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("Attribute", boost::bind(&i6engine::rpg::components::AttributeComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("Healthbar", boost::bind(&i6engine::rpg::components::HealthbarComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("ListInventory", boost::bind(&i6engine::rpg::components::ListInventoryComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("Name", boost::bind(&i6engine::rpg::components::NameComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("Quickslot", boost::bind(&i6engine::rpg::components::QuickslotComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("Slot", boost::bind(&i6engine::rpg::components::SlotComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("SlotInventory", boost::bind(&i6engine::rpg::components::SlotInventoryComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("ThirdPersonControl", boost::bind(&i6engine::rpg::components::ThirdPersonControlComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("UsableItem", boost::bind(&i6engine::rpg::components::UsableItemComponent::createC, _1, _2));
+		i6engine::api::EngineController::GetSingleton().getObjectFacade()->registerCTemplate("WeightInventory", boost::bind(&i6engine::rpg::components::WeightInventoryComponent::createC, _1, _2));
+#endif
 
 		// register ESC to close the application
 		i6engine::api::EngineController::GetSingletonPtr()->getInputFacade()->subscribeKeyEvent(i6engine::api::KeyCode::KC_ESCAPE, i6engine::api::KeyState::KEY_PRESSED, boost::bind(&i6engine::api::EngineController::stop, i6engine::api::EngineController::GetSingletonPtr()));
