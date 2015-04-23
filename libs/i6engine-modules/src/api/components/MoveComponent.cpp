d969121d70aaa8e2acb645b725c360c61346443a
Daniel Bonrath <Big-DB@web.de>
2015-04-23 12:59:51 +0200

    ISIXE-1410 added implementation of MoveComponent

--- libs/i6engine-modules/src/api/components/MoveComponent.cpp
+++ libs/i6engine-modules/src/api/components/MoveComponent.cpp
@@ -1,13 +1,16 @@
 #include "i6engine/api/components/MoveComponent.h"
 
+#include "i6engine/math/i6eMath.h"
+
 #include "i6engine/api/components/MovementComponent.h"
+#include "i6engine/api/components/PhysicalStateComponent.h"
 #include "i6engine/api/configs/ComponentConfig.h"
 #include "i6engine/api/objects/GameObject.h"
 
 namespace i6engine {
 namespace api {
 
-	MoveComponent::MoveComponent(const int64_t id, const attributeMap & params) : Component(id, params), _mc() {
+	MoveComponent::MoveComponent(const int64_t id, const attributeMap & params) : Component(id, params), _mc(), _index(0), _path() {
 		_objFamilyID = components::ComponentTypes::MoveComponent;
 		_objComponentID = components::ComponentTypes::MoveComponent;
 	}
@@ -25,6 +28,25 @@ namespace api {
 	}
 
 	void MoveComponent::Tick() {
+		if (_index < _path.size()) {
+			auto psc = getOwnerGO()->getGOC<PhysicalStateComponent>(components::ComponentTypes::PhysicalStateComponent);
+			if ((psc->getPosition() - _path[_index]).length() < 0.5) {
+				_index++;
+				if (_index == _path.size()) {
+					return;
+				}
+			}
+			double angle = Vec3::crossAngleSigned(math::rotateVector(Vec3(0.0, 0.0, 1.0), psc->getRotation()), _path[_index] - psc->getPosition(), Vec3(0, 1, 0));
+			auto mc = _mc.get();
+			if (angle > 0) {
+				mc->left();
+			} else if (angle < 0) {
+				mc->right();
+			}
+			if (std::abs(angle) < PI / 4.0) { // only move forward if the new target point is in a 45 degree range, otherwise only turn to the side just in place 
+				mc->forward();
+			}
+		}
 	}
 
 	void MoveComponent::Finalize() {
