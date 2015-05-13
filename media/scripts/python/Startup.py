eec0b6a18019f7d9bd99c3d48a88060b7a1c9aa0
Daniel Bonrath <Big-DB@web.de>
2015-05-13 15:32:20 +0200

    ISIXE-1362 added python script with Startup function

---
+++ media/scripts/python/Startup.py
@@ -0,0 +1,7 @@
+﻿import ScriptingRPGPython
+from ScriptingRPGPython import *
+
+# This method is called during first loading of a level in RPG
+def Startup():
+	insertNPCAtWaypoint("NPC01", "WP_001")
+	return
