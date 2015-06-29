0c9d54ebb524353f8741081eb860c0ba29bdad19
Daniel Bonrath <Big-DB@web.de>
2015-06-29 14:33:02 +0200

    ISIXE-909 improved build scripts

--- dependencies/build-common.bat
+++ dependencies/build-common.bat
@@ -5,12 +5,14 @@ Set CONFIG_BAT="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall
 
 echo %1
 
-IF "%1" == "downloadAndUnpack" (goto downloadAndUnpack %2 %3)
+IF "%1" == "downloadAndUnpack" (goto downloadAndUnpack)
 EXIT /B
 
 :downloadAndUnpack
+Set DOWNLOAD_URL=%4
+IF [%4] == [] Set DOWNLOAD_URL=http://www.clockwork-origins.de/dependencies/
 IF not exist %TMP_DIR% (mkdir %TMP_DIR%)
-IF not exist %TMP_DIR%\%2 (bitsadmin /transfer "myDownloadJob%2" /download /priority normal http://www.clockwork-origins.de/dependencies/%2 %TMP_DIR%\%2)
+IF not exist %TMP_DIR%\%2 (bitsadmin /transfer "myDownloadJob%2" /download /priority normal %DOWNLOAD_URL%%2 %TMP_DIR%\%2)
 cd %TMP_DIR%
 if exist %3 RD /S /Q "%2"
 winrar.exe x %2
