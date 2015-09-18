71a13b4c3788e39000b3559b38996ac815227b26
Daniel Bonrath <Big-DB@web.de>
2015-09-18 16:51:27 +0200

    ISIXE-1531 changed to dynamic build of wxWidgets on Windows

--- dependencies/build-wxWidgets.bat
+++ dependencies/build-wxWidgets.bat
@@ -11,20 +11,22 @@ call build-common.bat downloadAndUnpack %ARCHIVE% %BUILD_DIR%
 echo "Building wxWidgets"
 cd %BUILD_DIR%\build\msw
 
-MSBuild.exe wx_vc12.sln /p:Configuration=Release > NUL
+MSBuild.exe wx_vc12.sln /p:Configuration="DLL Release" > NUL
 
 echo "Installing wxWidgets"
 mkdir "%PREFIX%"
 mkdir "%PREFIX%/include"
+mkdir "%PREFIX%/bin"
 mkdir "%PREFIX%/lib"
 mkdir "%PREFIX%/include/wx"
-mkdir "%PREFIX%/lib/vc_lib"
-mkdir "%PREFIX%/lib/vc_lib/mswu"
+mkdir "%PREFIX%/lib/vc_dll"
+mkdir "%PREFIX%/lib/vc_dll/mswu"
 xcopy /S /Y "%BUILD_DIR%\include" "%PREFIX%\include\wx" > NUL
 xcopy /S /Y "%BUILD_DIR%\include\msvc" "%PREFIX%/include\wx" > NUL
 
-xcopy /S /Y "%BUILD_DIR%\lib\vc_lib\*.lib" "%PREFIX%\lib\" > NUL
-xcopy /S /Y "%BUILD_DIR%\lib\vc_lib\mswu" "%PREFIX%\lib\vc_lib\mswu" > NUL
+xcopy /S /Y "%BUILD_DIR%\lib\vc_dll\*.dll" "%PREFIX%\bin\" > NUL
+xcopy /S /Y "%BUILD_DIR%\lib\vc_dll\*.lib" "%PREFIX%\lib\" > NUL
+xcopy /S /Y "%BUILD_DIR%\lib\vc_dll\mswu" "%PREFIX%\lib\vc_dll\mswu" > NUL
 
 echo "Cleaning up"
 cd %DEP_DIR%
