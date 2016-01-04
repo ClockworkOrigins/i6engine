6f1527ec4c8838b9f5e8a77080ef8be8a5e4075f
Daniel Bonrath <Big-DB@web.de>
2016-01-04 10:19:35 +0100

    ISIXE-1656 added basic context menu

--- tools/particleEditor/src/widgets/MainWindow.cpp
+++ tools/particleEditor/src/widgets/MainWindow.cpp
@@ -5,6 +5,8 @@
 #include "widgets/WidgetParticleList.h"
 #include "widgets/WidgetRender.h"
 
+#include <QToolBar>
+
 namespace i6engine {
 namespace particleEditor {
 namespace widgets {
@@ -16,13 +18,25 @@ namespace widgets {
 
 		setWindowTitle(QString("ParticleEditor (v ") + QString::number(ISIXE_VERSION_MAJOR) + QString(".") + QString::number(ISIXE_VERSION_MINOR) + QString(".") + QString::number(ISIXE_VERSION_PATCH) + QString(")"));
 
-		gridLayout->addWidget(_particleListWidget, 0, 0);
-		gridLayout->addWidget(_tabWidget, 0, 1);
+		QToolBar * tb = new QToolBar(this);
+		QAction * playAction = tb->addAction(QIcon("../media/textures/control_play_blue.png"), "Play");
+
+		connect(playAction, SIGNAL(triggered()), _renderWidget, SLOT(play()));
+
+		gridLayout->addWidget(tb, 0, 0);
+
+		QWidget * hWidget = new QWidget(this);
+		QHBoxLayout * hLayout = new QHBoxLayout(hWidget);
+
+		gridLayout->addWidget(hWidget, 1, 0);
+
+		hLayout->addWidget(_particleListWidget);
+		hLayout->addWidget(_tabWidget);
 
 		_tabWidget->addTab(_renderWidget, "Render");
 
-		gridLayout->setColumnStretch(0, 1);
-		gridLayout->setColumnStretch(1, 3);
+		hLayout->setStretch(0, 1);
+		hLayout->setStretch(1, 3);
 	}
 
 	MainWindow::~MainWindow() {
