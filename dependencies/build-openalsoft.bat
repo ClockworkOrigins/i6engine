c401c152ca7f140758bd6e7cf62eff279b8c09de
Daniel Bonrath <bonne@clockwork-origins.de>
2016-07-16 10:23:09 +0200

    ISIXE-1865 improved OpenAL build script

--- dependencies/build-openalsoft.bat
+++ dependencies/build-openalsoft.bat
@@ -23,6 +23,8 @@ Set ARCHIVE=openal-soft-1.17.2.tar.bz2
 Set BUILD_DIR=%TMP_DIR%/openal-soft-1.17.2
 Set PREFIX=%DEP_DIR%/%ARCH_DIR%/OpenALSoft/
 
+IF EXIST %PREFIX% EXIT /B
+
 echo "Compile OpenAL Soft"
 
 call build-common.bat downloadAndUnpack %ARCHIVE% %BUILD_DIR%
